f2563123eaa8e65315dcda0fae0618df17d1b4bd
2023thong <thongpdpd06029@fpt.edu.vn>
2023-10-06 11:07:11 +0700

    trangkho

---
+++ app/src/main/java/gmail/com/qlcafepoly/AdminKho.java
@@ -0,0 +1,47 @@
+package gmail.com.qlcafepoly;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class AdminKho extends AppCompatActivity {
+    BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_kho);
+        bottomNavigationView = findViewById(R.id.bonava);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId()==R.id.kho){
+                    replaceFragment(new KhoFragment());
+                }
+                return true;
+            }
+        });
+
+
+
+    }
+
+
+
+
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.framg, fragment);
+        fragmentTransaction.commit();
+    }
+}
